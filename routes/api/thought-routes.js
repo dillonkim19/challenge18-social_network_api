9e7818c5b2d1519c06b2ef7b3321d5781b32eb7a
Dillon Kim <dillonkim@Dillons-Air.attlocal.net>
2023-02-16 16:11:50 -0800

    add routes and readme

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -4,36 +4,120 @@ const { Thought, Reaction} = require('../../models')
 
 //TODO: ROUTE TO GET ALL THOUGHTS
 router.get('/', (req,res)=> {
-
+    Thought.find({}, (err, thoughts) => {
+        if (err) {
+            res.send(err)
+        }
+        res.status(200).json(thoughts)
+    }).populate('reactions')
 })
 
 //TODO: ROUTE TO CREATE A NEW THOUGHT
 router.post('/', (req,res)=> {
-
+    Thought.create({
+        thoughtText: req.body.thoughtText,
+        username: req.body.username,
+    }, (err, thought) => {
+        if (err) {
+            res.send(err)
+        }
+        res.status(200).json("Thought added")
+    })
 });
 
 //TODO: ROUTE TO GET SINGLE THOUGHT BASED ON THOUGHT ID
 router.get('/:thoughtId', (req,res)=> {
-
+    Thought.findById(req.params.thoughtId, (err, thought) => {
+        if (err) {
+            res.send(err)
+        }
+        res.status(200).json(thought)
+    })
 })
 
 //TODO: ROUTE TO UPDATE A THOUGHT
 router.put('/', (req,res)=> {
-
+    const options = {new: true}
+    Thought.findOneAndUpdate(req.body.filter, req.body.update, options, (err, thought) => {
+        if (err) {
+            console.log(err)
+            res.send(err)
+        }
+        res.status(200).json(thought)
+    })
 })
 
 //TODO: ROUTE TO DELETE A THOUGHT BASED ON THOUGHT ID
 router.delete('/:thoughtId', (req,res)=> {
-
+    Thought.findByIdAndDelete(req.params.thoughtId, (err, thoughts) => {
+        if (err) {
+            console.log(err)
+            res.send(err)
+        }
+        res.status(200).json("Thought Deleted!")
+    })
 });
 
 //TODO: ROUTE TO ADD REACTION TO A THOUGHT
 router.post('/:thoughtId/reactions', (req,res)=> {
 
+    Reaction.create({
+        reactionBody: req.body.reactionBody,
+        username: req.body.username,
+    }, (reactionErr, reaction) => {
+        Thought.findByIdAndUpdate(
+            req.params.thoughtId,
+            { $push: { reactions: reaction }},
+            (thoughtErr, thought) => {
+                if (thoughtErr) {
+                    console.log(thoughtErr)
+                    res.send(thoughtErr)
+                }
+                console.log('Reaction: ', req.body.reactionBody, ' added!')
+                res.status(200).json('Reaction Added!')
+            }
+        )
+    })
+
+    // let newReaction = new Reaction({
+    //     reactionBody: req.body.reactionBody,
+    //     username: req.body.username,
+    // })
+    // newReaction.save((err, result) => {
+    //     Thought.findByIdAndUpdate(
+    //         req.params.thoughtId,
+    //         {$push: { reactions: newReaction }},
+    //         (err, thought) => {
+    //             if (err) {
+    //                 console.log(err)
+    //                 res.send(err)
+    //             }
+    //             console.log('Reaction: ', newReaction.reactionBody, 'added to thought')
+    //             res.status(200).json(thought)
+    //         }
+    //     )
+    // })
+
 });
 
 //TODO: ROUTE TO DELETE A REACTION ON A THOUGHT
 router.delete('/:thoughtId/reactions/:reactionId', (req,res)=> {
+    Reaction.findByIdAndDelete(req.params.reactionId, (reactionErr, reaction) => {
+
+        Thought.findByIdAndUpdate(
+            req.params.thoughtId,
+            { $pull: {reaction} },
+            (err, thought) => {
+                if (err) {
+                    console.log(err)
+                    res.send(err)
+                }
+                console.log(reaction)
+                console.log('Reaction: ', req.params.reactionId, ' deleted!')
+                res.status(200).json(thought)
+            }
+        )
+    })
 
 })
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,37 +4,95 @@ const {User} = require("../../models")
 
 //TODO - ROUTE THAT GETS ALL THE USERS, include friends?
 router.get('/', (req,res)=> {
-
+    User.find({}, (err, users) => {
+        if (err) {
+            console.log(err)
+            res.send(err)
+        }
+        res.status(200).json(users)
+    }).populate('friends')
+    .populate('thoughts')
 })
 
 //TODO - ROUTE THAT CREATES A NEW USER
 router.post('/', (req,res)=> {
-
+    User.create({
+        username: req.body.username,
+        email: req.body.email
+    }, (err, user) => {
+        if (err) {
+            console.log(err)
+            res.send(err)
+        }
+        res.status(200).json(user)
+    })
 });
 
 //TODO - ROUTE THAT GETS A SINGLE USER BASED ON USER ID
 router.get('/:userId', (req,res) => {
-
+    User.findById(req.params.userId, (err, user) => {
+        if (err) {
+            res.send(err)
+        }
+        res.status(200).json(user)
+    }).populate('thoughts', 'friends')
 })
 
 //TODO - ROUTE THAT UPDATES A SINGLE USER
 router.put('/:userId', (req,res)=> {
-
+    const options = {new:true}
+    User.findByIdAndUpdate(req.params.userId, req.body, options, (err, user) => {
+        if (err) {
+            console.log(err)
+            res.send(err)
+        }
+        res.status(200).json(user)
+    })
 })
 
 //TODO - ROUTE THAT DELETES A SINGLE USER BASED ON USER ID
 router.delete('/:userId', (req,res)=> {
-
+    User.findByIdAndDelete(req.params.userId, (err, user) => {
+        if (err) {
+            console.log(err)
+            res.send(err)
+        }
+        res.status(200).json("User Deleted!")
+    })
 });
 
 //TODO - ROUTE THAT ADDS A FRIEND TO A USER
 router.put('/:userId/friends/:friendId', (req,res)=> {
-
+    User.findById(req.params.friendId, (friendErr, friend) => {
+      User.findByIdAndUpdate(
+          req.params.userId,
+          { $push: { friends: friend }},
+          (userErr, user) => {
+            if (userErr) {
+                console.log(userErr)
+                res.send(userErr)
+            }
+            console.log('Friend:', friend.username ,' added!')
+            res.status(200).json('Friend added!')
+          }
+      )
+    })
 })
 
 //TODO - ROUTE THAT DELETES A FRIEND FROM A USER'S FRIENDS, DONT DELETE THE FRIEND AS A USER THOUGH!
 router.delete('/:userId/friends/:friendId', (req,res)=> {
-  
+    User.findByIdAndUpdate(
+        req.params.userId,
+        { $pull: {friends: req.params.friendId}},
+        (userErr, user) => {
+            if (userErr) {
+                console.log(userErr)
+                res.send(userErr)
+            }
+            console.log('Friend: deleted!')
+            res.status(200).json('Friend deleted!')
+        }
+    )
 });
 
 module.exports = router;
